045c84764c5569ac3c084a4d55d75e09f51f7c54
RACE - Rasmus Cederdorff <rasmus@cederdorff.com>
2024-12-06 12:52:33 +0100

    Adjust MapContainer height to 90vh for improved layout

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -13,7 +13,7 @@ export default function Map() {
   }, []);
 
   return (
-    <MapContainer center={[51.505, -0.09]} zoom={13} style={{ height: "100vh", width: "100%" }}>
+    <MapContainer center={[51.505, -0.09]} zoom={13} style={{ height: "90vh", width: "100%" }}>
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
