3f76e7dc91807b8087988d2b9f09744e3d611606
RACE - Rasmus Cederdorff <rasmus@cederdorff.com>
2024-12-06 12:52:07 +0100

    Initialize React project with Vite, add basic components and configuration

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Map from "./components/Map";
+
+function App() {
+  return (
+    <main>
+      <h1>Vite + React + React Leaflet</h1>
+      <Map />
+    </main>
+  );
+}
+
+export default App;
---
+++ src/components/Map.jsx
@@ -0,0 +1,28 @@
+import "leaflet/dist/leaflet.css";
+import { useEffect, useState } from "react";
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+
+export default function Map() {
+  const [locations, setLocations] = useState([]);
+
+  useEffect(() => {
+    // Fetch locations from the JSON file
+    fetch("/locations.json")
+      .then(response => response.json())
+      .then(data => setLocations(data));
+  }, []);
+
+  return (
+    <MapContainer center={[51.505, -0.09]} zoom={13} style={{ height: "100vh", width: "100%" }}>
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      {locations.map(location => (
+        <Marker key={location.id} position={[location.lat, location.lng]}>
+          <Popup>{location.name}</Popup>
+        </Marker>
+      ))}
+    </MapContainer>
+  );
+}
